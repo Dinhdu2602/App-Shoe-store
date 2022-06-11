92d9b883957324329e5fdd94f1610149173c4fa6
Dinhdu2602 <20520159@gm.uit.edu.vn>
2022-06-12 01:14:58 +0700

    App build 2nd change

---
+++ src/screens/CheckoutScreen/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'; 8K (gzipped: 3.2K)
+import { Text, View, StyleSheet } from 'react-native';
+
+interface CheckoutScreenProps {}
+
+const CheckoutScreen = (props: CheckoutScreenProps) => {
+    return (
+        <View>
+            <Text>CheckoutScreen</Text>
+        </View>
+    );
+};
+
+export default CheckoutScreen;
